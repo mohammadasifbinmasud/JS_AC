181d08e70caf08f9b433e9eafedab9161f67a134
Mohammad Asif Bin Masud <mohammadasifbinmasud@gmail.com>
2024-12-21 22:00:06 +0600

    JS revision at Asif's home

---
+++ theme.js
@@ -0,0 +1,96 @@
+let body = document.querySelector(".body");
+let theme = "light";
+let button = document.querySelector(".button");
+
+
+
+
+const changeTheme = () => {
+    if (theme === "light") {
+        body.style.backgroundColor = "black";
+        button.innerHTML = "light";
+        theme = "dark";
+    } else if (theme === "dark") {
+        body.style.backgroundColor = "white";
+        button.innerHTML = "dark";
+        theme = "light";
+    }
+}
+
+
+
+/* 
+//CONDITIONALS
+let num = prompt("Enter a number: ");
+num = parseInt(num);
+if (num % 5 ===0){
+    body.style.backgroundColor = "cyan";
+} 
+*/
+
+
+
+/*
+//STRINGS
+let fullName = window.prompt("Enter full name: ");
+
+let firstName = fullName.slice(0, fullName.indexOf(" "));
+let lastName = fullName.slice(fullName.indexOf(" ") + 1, fullName.length + 1);
+
+let letter = firstName.charAt(0);
+letter = letter.toLowerCase();
+let ext = firstName.slice(1);
+firstName = letter + ext;
+
+letter = lastName.charAt(0);
+letter = letter.toLowerCase();
+ext = lastName.slice(1);
+lastName = letter + ext;
+
+usrName = firstName + lastName;
+let len = usrName.length;
+
+usrName = usrName + len;
+
+let txt = document.querySelector(".p");
+txt.innerText= `Your Username is: @${usrName}`;
+*/
+
+
+
+/*
+//STRINGS
+let fullName = window.prompt("Enter full name: ");
+fullName = fullName.trim();
+fullName = fullName.toLowerCase();
+let firstName = fullName.slice(0, fullName.indexOf(" "));
+let lastName = fullName.slice(fullName.indexOf(" ") + 1, fullName.length + 1);
+
+let usrName = firstName + lastName;
+let leng = usrName.length;
+usrName = usrName + leng;
+
+let txt = document.querySelector(".p");
+txt.innerText= `Your Username is: @${usrName}`;
+*/
+
+
+
+let prices = [250, 645, 300, 900, 50];
+
+for (let i = 0; i < prices.length; i++) {
+    prices[i] = prices[i] * (1 - 0.1);
+}
+
+console.log(prices);
+
+
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+companies.splice(0,1);//deletes the element of index - 0
+console.log(companies);
+
+companies.splice(1, 1, "Ola");
+console.log(companies);
+
+companies.splice(5, 0, "Amazon");
+console.log(companies);
