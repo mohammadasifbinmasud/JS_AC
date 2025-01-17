4fbbe224636f2371931f2cce4fc9d4ec10311f8b
Moinul Islam <moinulislam159632@gmail.com>
2025-01-18 00:41:05 +0600

    found---> The problem was that the API isn't working correctly

--- projects/Currency_Converter/app.js
+++ projects/Currency_Converter/app.js
@@ -7,7 +7,7 @@ const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
 
 for (let select of dropdowns) {
-    for (currCode in countryList) {
+    for (let currCode in countryList) {
         let newOption = document.createElement("option");
         newOption.innerText = currCode;
         newOption.value = currCode;
